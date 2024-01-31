215cd40247020d262d4135b2310b71179d0fb27a
冯睿 <140141758+SKYDOGGGG@users.noreply.github.com>
2024-01-31 15:08:44 +0800

    Update alstm2.py

--- alstm2/alstm2.py
+++ alstm2/alstm2.py
@@ -1,34 +1,36 @@
 """
 2024.1.24 15:00
-Alstm2
+ALSTM2
 
-引入Bahdanau Attention机制，模型由卷积网络、RNN（GRU）以及注意力机制
+引入Bahdanau Attention机制，模型由卷积网络和引入注意力机制的RNN（GRU/LSTM）组成
 
 Attention用于计算注意力分数和上下文向量。
 它使用三个线性层（w_q, w_k, w_v）来计算查询（query）和键（keys）之间的关系，然后产生注意力分数和上下文向量。
 上下文向量是每个股票的每个隐藏层在时间维度上的加权值，权重由注意力分数决定，格式为[batch_size, hid_size]。
 
-整体上，输入数据同时通过：1.卷积神经网络（包含卷积层），然后通过一个全连接层；2. GRU 网络，其输出与最后一个隐藏状态一起被送入注意力模块，再经过 Dropout 层。
+整体上，输入数据同时通过：
+1.卷积神经网络（包含卷积层），然后通过一个全连接层；
+2. GRU 网络，其输出与最后一个隐藏状态（在双向rnn中，是最后两个隐藏状态）一起被送入注意力模块，再经过 Dropout 层。
 
 卷积网络和 GRU 网络的输出被合并并通过最后一个全连接层，产生最终的输出。
 
-模型传参有一定变化，需要传入时间长度time_period。
 
 """
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.init as init
 import numpy as np
 
 
 class Attention(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, input_size, bi_ind=1):
         super(Attention, self).__init__()
-        self.hid_size = input_size
-        self.w_q = nn.Linear(in_features=input_size, out_features=input_size, bias=False)
-        self.w_k = nn.Linear(in_features=input_size, out_features=input_size, bias=False)
-        self.w_v = nn.Linear(in_features=input_size, out_features=1, bias=False)
+        self.hid_size = input_size * bi_ind
+        self.w_q = nn.Linear(in_features=self.hid_size, out_features=self.hid_size, bias=False)
+        self.w_k = nn.Linear(in_features=self.hid_size, out_features=self.hid_size, bias=False)
+        self.w_v = nn.Linear(in_features=self.hid_size, out_features=1, bias=False)
 
     def forward(self, query, keys):
         query = query.unsqueeze(0)  # [1, batch_size, hid_size]
@@ -41,17 +43,18 @@ class Attention(nn.Module):
         return context_vector, attention_weights
 
 
-class ALSTMModel(nn.Module):
-    def __init__(self, d_feat=6, time_period=60, hidden_size1=128, hidden_size2=64, num_layers=2, dropout=0.0,
-                 rnn_type="GRU"):
+class ALSTM2(nn.Module):
+    def __init__(self, d_feat=6, hidden_size1=128, hidden_size=64, num_layers=2, dropout=0.0,
+                 rnn_type="GRU", 双向=False, **kwargs):
         super().__init__()
         self.hid_size1 = hidden_size1
-        self.hid_size2 = hidden_size2
-        self.time_period = time_period
+        self.hid_size2 = hidden_size
         self.input_size = d_feat
         self.dropout = dropout
-        self.attention = Attention(input_size=self.hid_size2)
-        self.rnn_type = rnn_type
+        self.bidirectional = 双向
+        self.bi_ind = 1 + int(双向)
+        self.attention = Attention(input_size=self.hid_size2, bi_ind=self.bi_ind)
+        self.rnn_type = rnn_type.lower()
         self.rnn_layer = num_layers
         self._build_model()
 
@@ -70,8 +73,8 @@ class ALSTMModel(nn.Module):
             nn.ReLU(),
         )
 
-        self.fc1 = nn.Linear(in_features=self.time_period, out_features=1)
-        self.fc_out = nn.Linear(in_features=self.hid_size2 * 2, out_features=1)
+        self.fc1 = nn.AdaptiveAvgPool1d(1)
+        self.fc_out = nn.Linear(in_features=self.hid_size2 * (1 + self.bi_ind), out_features=1)
 
         self.rnn = klass(
             input_size=self.input_size,
@@ -79,6 +82,7 @@ class ALSTMModel(nn.Module):
             num_layers=self.rnn_layer,
             batch_first=True,
             dropout=self.dropout,
+            bidirectional=self.bidirectional,
         )
 
         self.lstm_drop = nn.Dropout(p=self.dropout)
@@ -91,23 +95,34 @@ class ALSTMModel(nn.Module):
 
         inputs_lstm = inputs.clone().permute(0, 2, 1)  # [input_size, batch_size, input_day]
 
-        rnn_out, (h, c) = self.rnn(inputs_lstm)
+        if self.rnn_type.lower() == "lstm":
+            rnn_out, (h, c) = self.rnn(inputs_lstm)
+            if self.bidirectional:
+                h = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+            else:
+                h = h[-1, :, :]
+        elif self.rnn_type.lower() == "gru":
+            rnn_out, h = self.rnn(inputs_lstm)
+            if self.bidirectional:
+                h = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+            else:
+                h = h[-1, :, :]
+        else:
+            raise ValueError("unknown rnn_type `%s`" % self.rnn_type)
 
         context, attention_score = self.attention(h, rnn_out)
 
         out_lstm = self.lstm_drop(context)
 
-        out = torch.cat((conv_out, out_lstm.unsqueeze(2)), dim=2)
-        out = out.view(len(out), -1)
+        out = torch.cat((conv_out.squeeze(-1), out_lstm), dim=-1)
         out = self.fc_out(out)
         return out[...]
 
+
 # test
-# if __name__ == "__main__":
-#     x = torch.randn(20000, 20, 300)
-#     model = ALSTMModel(d_feat=300, time_period=20, hidden_size1=128, hidden_size2=64, num_layers=2, dropout=0.6,
-#                        rnn_type="GRU")
-#     y = model(x)
-#     y = y.detach().numpy()
-#     print(y.shape)
-#     print(y)
+if __name__ == "__main__":
+    x = torch.randn(1000, 20, 100)
+    model = ALSTM2(d_feat=100, time_period=20, hidden_size1=128, hidden_size2=64, num_layers=2, dropout=0.6,
+                   rnn_type="gru", 双向=True)
+    y = model(x)
+    print(y.shape)
